a585d183e1b802a3caf7c2650814a9038a2ece0c
Jake Jezek <jake@traced-it.app>
2025-02-12 14:32:57 +0100

    v1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "app.traced_it"
         minSdk = 25
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         vectorDrawables {
             useSupportLibrary = true
