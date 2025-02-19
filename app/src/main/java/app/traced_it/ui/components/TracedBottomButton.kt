3c5f84fdea8a3111587765f4bd003f2e32224fa5
Jake Jezek <jake@traced-it.app>
2025-02-19 18:54:56 +0100

    TracedBottomButton: make it bold

--- app/src/main/java/app/traced_it/ui/components/TracedBottomButton.kt
+++ app/src/main/java/app/traced_it/ui/components/TracedBottomButton.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import app.traced_it.ui.theme.AppTheme
@@ -38,7 +39,7 @@ fun TracedBottomButton(
                 disabledContentColor = MaterialTheme.colorScheme.onSecondary
             ),
         ) {
-            Text(text)
+            Text(text, fontWeight = FontWeight.Bold)
         }
     }
 }
