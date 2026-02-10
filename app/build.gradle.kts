e9ee31a3c475654c392cc7dd3b5ed3bfcf2c68d9
Jake Jezek <jake@traced-it.app>
2026-02-10 19:39:14 +0100

    lint

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,6 @@ android {
         aidl = false
         buildConfig = true
         compose = true
-        renderScript = false
         shaders = false
     }
     packaging {
@@ -82,7 +81,7 @@ android {
     }
     sourceSets {
         // Adds exported schema location as test app assets.
-        getByName("androidTest").assets.srcDir("$projectDir/schemas")
+        getByName("androidTest").assets.directories.add("$projectDir/schemas")
     }
     lint {
         disable += "MissingTranslation"
